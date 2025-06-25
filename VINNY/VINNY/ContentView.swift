6960faf24884f957b2cb8166214698d410ed508c
taebin2 <109895271+taebin2@users.noreply.github.com>
2025-06-25 14:57:21 +0900

    비니 프로젝트 시작

---
+++ VINNY/VINNY/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VINNY
+//
+//  Created by 한태빈 on 6/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VINNY/VINNY/VINNYApp.swift
@@ -0,0 +1,17 @@
+//
+//  VINNYApp.swift
+//  VINNY
+//
+//  Created by 한태빈 on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct VINNYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
