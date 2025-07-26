bbe73fcc088cd92c190a5c4460b0bc3190e88f77
g4oo0 <117704977+g4oo0@users.noreply.github.com>
2025-07-26 23:33:12 +0900

    design: 커뮤니티 디자인 수정

--- VINNY/VINNY/Source/Feature/Community/CommuViewModel/PostUploadViewModel.swift
+++ VINNY/VINNY/Source/Feature/Community/CommuViewModel/PostUploadViewModel.swift
@@ -8,8 +8,9 @@
 import Foundation
 
 final class PostUploadViewModel: ObservableObject {
-    @Published var title: String = ""
-    @Published var content: String = ""
-    @Published var brand: String = ""
-    @Published var shoptag: String = ""
+    @Published var title: String = "" // 제목
+    @Published var content: String = "" // 내용
+    @Published var styles: [String] = []
+    @Published var brand: String = "" // 브랜드 입력
+    @Published var shoptag: String = "" // 스타일 선택
 }
