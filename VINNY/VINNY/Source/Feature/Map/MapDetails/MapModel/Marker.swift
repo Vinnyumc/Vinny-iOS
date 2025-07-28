6a13b6729d6308be51bc89735203cbd99d5821ae
g4oo0 <117704977+g4oo0@users.noreply.github.com>
2025-07-28 17:25:58 +0900

    design: MapView 디자인 수정

--- VINNY/VINNY/Source/Feature/Map/MapDetails/MapModel/Marker.swift
+++ VINNY/VINNY/Source/Feature/Map/MapDetails/MapModel/Marker.swift
@@ -8,9 +8,17 @@
 import Foundation
 import MapKit
 
-struct Marker: Identifiable {
+struct Marker: Identifiable, Equatable {
     let id = UUID()
     let coordinate: CLLocationCoordinate2D
     let title: String
     let category: Category
+    
+    static func == (lhs: Marker, rhs: Marker) -> Bool {
+        return lhs.id == rhs.id &&
+               lhs.title == rhs.title &&
+               lhs.category == rhs.category &&
+               lhs.coordinate.latitude == rhs.coordinate.latitude &&
+               lhs.coordinate.longitude == rhs.coordinate.longitude
+    }
 }
